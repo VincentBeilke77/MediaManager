2309d7b10c7f80dc34c9bcb5ea7252f9555b83e6
Vincent Beilke <vincent.beilke@gmail.com>
2023-02-17 16:11:00 -0600

    Add project files.

---
+++ MediaManager.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaManager.Domain.Entities
+{
+    internal class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        #region BaseEntity Constructor
+        public BaseEntity(int id) 
+        {
+            Id = id;
+        }
+        #endregion
+
+        #region BaseEntity Overrides
+        public override string ToString()
+        {
+            return $"{Id}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (GetType() != obj.GetType()) return false;
+
+            var be = (BaseEntity) obj;
+            return be.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ MediaManager.Domain/Entities/Movie.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaManager.Domain.Entities
+{
+    internal class Movie : BaseEntity
+    {
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The movie title can only be 50 characters long.")]
+        public string Title { get; set; }
+        [StringLength(255, ErrorMessage = "The short description can only be 255 characters long.")]
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public int RunTime { get; set; }
+        public int ReleaseYear { get; set; }
+        public bool Favorite { get; set; }
+        public int RatingId { get; set; }
+
+        #region Movie Constructors
+        public Movie(int id, string title) : base(id) 
+        {
+            Title = title;
+        }
+
+        public Movie(int id, string title, string shortDescription)
+            : this(id, title)
+        {
+            ShortDescription = shortDescription;
+        }
+
+        public Movie(int id, string title, string shortDescription, string longDescription) 
+            : this(id, title, shortDescription)
+        {
+            LongDescription = longDescription;
+        }
+
+        public Movie(int id, string title, string shortDescription, string longDescription
+            , int runTime)
+            : this(id, title, shortDescription, longDescription)
+        {
+            RunTime = runTime;
+        }
+
+        public Movie(int id, string title, string shortDescription, string longDescription,
+            int runTime, int releaseYear)
+            : this(id, title, shortDescription, longDescription, runTime)
+        {
+            ReleaseYear = releaseYear;
+        }
+
+        public Movie(int id, string title, string shortDescription, string longDescription,
+            int runTime, int releaseYear, bool favorite)
+            : this(id, title, shortDescription, longDescription, runTime, releaseYear)
+        {
+            Favorite = favorite;
+        }
+
+        public Movie(int id, string title, string shortDescription, string longDescription,
+            int runTime, int releaseYear, bool favorite, int ratingId)
+            : this(id, title, shortDescription, longDescription, runTime, releaseYear,
+                favorite)
+        {
+            RatingId = ratingId;
+        }
+        #endregion
+
+        #region Movie Overrides
+        public override string ToString()
+        {
+            return $"{base.ToString}:{Title}:{RunTime}:{ReleaseYear}:{Favorite}:{RatingId}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (GetType() != obj.GetType()) return false;
+
+            var movie = (Movie) obj;
+            return movie.Id == Id && movie.Title.Equals(Title);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Title.GetHashCode();
+        }
+        #endregion
+    }
+}
